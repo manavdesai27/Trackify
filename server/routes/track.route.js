41113b8deaed358b622818e5eca3f7d0f1956bdb
Manav Desai <manav27202@gmail.com>
2022-07-26 00:23:07 +0530

    Multi track

--- server/routes/track.route.js
+++ server/routes/track.route.js
@@ -3,10 +3,12 @@ const {
     postTrack,
     deleteTracks,
     editTrack,
+    multiTrack,
     enableNotifications
 } = require('../controllers/track.controller');
 
 router.route("/track").post(postTrack);
+router.route("/multitrack").post(multiTrack);
 router.route("/delete/tracks").post(deleteTracks);
 router.route("/track/:id").post(editTrack);
 router.route("/notif/track/:id").post(enableNotifications);
